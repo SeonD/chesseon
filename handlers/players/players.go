a8d049ff07213c1004507a87e51508275a002849
Seondong Kim <playingnow@gmail.com>
2018-11-04 23:29:24 -0500

    Add POST, GET /player

--- handlers/players/players.go
+++ handlers/players/players.go
@@ -1,1 +1,48 @@
 package players
+
+import (
+	"net/http"
+
+	"github.com/SeonD/chesseon/models"
+	"github.com/gin-gonic/gin"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// Create a player
+func Create(c *gin.Context) {
+	db := c.MustGet("db").(*mgo.Database)
+
+	player := models.Player{}
+	err := c.Bind(&player)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	err = db.C(models.CollectionPlayer).Insert(player)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"created": true,
+	})
+}
+
+func GetById(c *gin.Context) {
+	db := c.MustGet("db").(*mgo.Database)
+	player := models.Player{}
+	oID := bson.ObjectIdHex(c.Param("_id"))
+	err := db.C(models.CollectionPlayer).FindId(oID).One(&player)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"_id":  player.ID,
+		"name": player.Name,
+	})
+}
--- main.go
+++ main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"net/http"
 
+	"github.com/SeonD/chesseon/db"
+	"github.com/SeonD/chesseon/handlers/players"
+	"github.com/SeonD/chesseon/middlewares"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	// Port that runs the app
+	Port = "3030"
+)
+
+func init() {
+	db.Connect()
+}
+
 func main() {
 	router := gin.Default()
 
+	router.Use(middlewares.Connect)
+	router.Use(middlewares.ErrorHandler)
 	router.Use(static.Serve("/", static.LocalFile("./views", true)))
 
 	api := router.Group("/api")
@@ -19,6 +33,12 @@ func main() {
 				"message": "pong",
 			})
 		})
+
+		player := api.Group("/players")
+		{
+			player.POST("/", players.Create)
+			player.GET("/:_id", players.GetById)
+		}
 	}
 
 	router.Run(":3030")
